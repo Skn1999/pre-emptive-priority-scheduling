0c722f5587a5692963462f4045401cd78b8e08be
Skn1999 <31825628+Skn1999@users.noreply.github.com>
2019-03-31 09:41:00 +0530

    Update priority_scheduling.c

--- priority_scheduling.c
+++ priority_scheduling.c
@@ -1,26 +1,27 @@
 #include<unistd.h>
 #include<stdio.h>
 
-int LimitOfProcess;
+int Limit;
 const int CONTEXT_SWITCH_TIME = 2;
 
 //processStatus = 1 means completed.
 
 struct process
 {
-	int processId, arrivalTime, burstTime, waitingTime, turnAroundTime, priority, processStatus;
+	int processId, arrivalTime, burstTime, waitingTime, turnAroundTime, priority;
+	int processStatus, remainingTime;
 };
 
-void prioritySorting(struct process *processQueue)
+void arrivalTimeSorting(struct process *processQueue)
 
 {
 
 	struct process temp;
-	for (int i = 0; i < LimitOfProcess ; i++)
+	for (int i = 0; i < Limit ; i++)
 	{
-		for(int j = i; j < LimitOfProcess ; j++)
+		for(int j = i+1; j < Limit ; j++)
 		{
-			if(processQueue[j].priority < processQueue[i].priority )
+			if(processQueue[j].arrivalTime < processQueue[i].arrivalTime )
 			{
 				temp = processQueue[j];
 				processQueue[j] = processQueue[i];
@@ -30,15 +31,33 @@ void prioritySorting(struct process *processQueue)
 	} 
 }
 
+int scheduler(struct process *processQueue, int time, int currentProcess)
+{
+
+        for (int i = 0 ; i < Limit+1 ; i++)
+        {
+                if(processQueue[i].arrivalTime <= time && processQueue[currentProcess].priority > processQueue[i].priority && processQueue[i].processStatus != 1)
+                {
+                        currentProcess = i;
+                }
+        }
+        return processQueue[currentProcess].processId;	
+}
+
 int main()
 {
 
-	int totalBurstTime=0, pid = 1;
+	int totalBurstTime=0, pid = 1, maxArrivalTime=0;
+	
 	printf("\nEnter the total number of processes: ");
-	scanf("%d", &LimitOfProcess);
-	struct process processQueue[LimitOfProcess];
+	scanf("%d", &Limit);
+	struct process processQueue[Limit+1];
 
-	for (int i = 0 ; i<LimitOfProcess ; i++, pid++)
+	//variable for selecting among the process which will be executed next
+	int currentProcess = Limit+1;
+	processQueue[Limit+1].priority = 1,000;
+
+	for (int i = 0 ; i<Limit ; i++, pid++)
 	{
 
 		processQueue[i].processId = pid;
@@ -54,18 +73,32 @@ int main()
 
 		//priority
 		printf("\nEnter the priority: ");
-		scanf("%d", &processQueue[i].priority);	
+		scanf("%d", &processQueue[i].priority);
+		
+		totalBurstTime += processQueue[i].priority;	
 
 	}
 
-	//sorting according to priority
-	prioritySorting(&processQueue);
+	//sorting according to arrival time
+	arrivalTimeSorting(processQueue);
 	
-	//printing of processes according to priority
-	for (int i =0 ; i< LimitOfProcess ; i++)
+	for(int i =0; i< Limit; i++)
 	{
-		printf("\nProcess id %d", processQueue[i].processId);
+	        if(processQueue[i].arrivalTime > maxArrivalTime)
+	                {
+	                        maxArrivalTime = processQueue[i].arrivalTime;
+			}
 	}
+	
+	printf("%d", maxArrivalTime);
+	 
+
+	/*for(int time = 0; time < totalBurstTime; )
+	{
+	        currentProcess = scheduler(processQueue, time, currentProcess);
+	        
+	        processQueue[currentProcess].burstTime--;	
+	}*/
 
 	return 0;
 }
